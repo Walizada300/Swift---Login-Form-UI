305401c42727562efabd86f2137417a0d43c30bb
Walizada300 <walizada.300@gmail.com>
2025-06-28 09:23:59 +0430

    Initial Commit

---
+++ AppTemplate/AppTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTemplateApp.swift
+//  AppTemplate
+//
+//  Created by RTA Media on 6/28/25.
+//
+
+import SwiftUI
+
+@main
+struct AppTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppTemplateTests/AppTemplateTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppTemplateTests.swift
+//  AppTemplateTests
+//
+//  Created by RTA Media on 6/28/25.
+//
+
+import Testing
+@testable import AppTemplate
+
+struct AppTemplateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
